7db2224fa501017d59ba972fbd535d84a8a704b7
WalidurTanjim <mohammadtanjim91@gmail.com>
2026-03-19 23:58:32 +0600

    404 not found route created

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import { userRoutes } from "./modules/user/user.routes";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import { todoRouter } from "./modules/todo/todo.routes";
+import { authRoutes } from "./modules/auth/auth.routes";
 
 const app = express();
 
@@ -18,7 +19,19 @@ app.use("/users", userRoutes);
 // todo CRUD
 app.use("/todos", todoRouter);
 
+// auth 
+app.use("/auth", authRoutes);
+
 // middleware
 app.use(globalErrorHandler);
+
+// 404 not found route
+app.use((req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+    data: req?.path
+  });
+});
 
 export default app;
